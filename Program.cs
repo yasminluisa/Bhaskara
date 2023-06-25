b2487eb00ad8a6a1841f98cd4e8dda4a7e2686c8
yasminluisa <126296748+yasminluisa@users.noreply.github.com>
2023-06-25 19:46:11 -0300

    Adiciona solução do exercício Bhaskara

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿double a, b, c, delta, x1, x2;
+
+Console.WriteLine("-- Equação do seguundo grau --\n");
+
+Console.Write("(a): ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("(b): ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("(c): ");
+c = Convert.ToDouble(Console.ReadLine());
+
+if (a == 0)
+{
+    Console.WriteLine(" Não é uma equção do segundo grau!");
+}
+else
+{
+    delta = b * b - 4 * a * c;
+    if (delta < 0)
+    {
+        Console.WriteLine($"Como delta = {delta:N2}, a equação não possui raízes reais!");
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+        Console.WriteLine($"x1 = {x1:N2} e x2 = {x2:N2}");
+    }
+}
